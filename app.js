02ece468b36a6c3486369a516fc187262a7ea7d2
Marco Agrippa Dev <ma.agippa@gmail.com>
2023-05-05 01:07:43 -0300

    Relogio Analogico Finalizado

---
+++ app.js
@@ -0,0 +1,26 @@
+const secondsHand = document.getElementById("seconds-hand");
+
+const minutesHand = document.getElementById("minutes-hand");
+
+const hoursHand = document.getElementById("hours-hand");
+
+
+function getTime() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+
+    // console.log(hours);
+    // console.log(minutes);
+    // console.log(seconds);
+
+    const timeInterval = 6;
+
+    secondsHand.style.transform = 'rotate(' + (seconds * timeInterval) + 'deg)'
+    minutessHand.style.transform = 'rotate(' + (minutes * timeInterval + seconds / 10) + 'deg)'
+    hourssHand.style.transform = 'rotate(' + (hours * 30 + minutes / 2) + 'deg)'
+
+}
+
+setInterval(getTime, 100);
